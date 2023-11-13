d0115f8557466c8b70f6d3e1c30ff2039c1e14ef
LurdRaiden <everethduncan@gmail.com>
2023-11-13 10:37:26 -0500

    SystemUI: Add QS tile layout settings [2/2]

--- src/com/pixeldust/settings/quicksettings/QuicksettingsSettings.java
+++ src/com/pixeldust/settings/quicksettings/QuicksettingsSettings.java
@@ -7,12 +7,15 @@
 package com.pixeldust.settings.quicksettings;
 
 import android.os.Bundle;
-
+import android.content.Context;
+import android.provider.Settings;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
+import android.os.UserHandle;
+import android.content.ContentResolver;
 
 @SearchIndexable
 public class QuicksettingsSettings extends SettingsPreferenceFragment {
@@ -21,6 +24,18 @@ public class QuicksettingsSettings extends SettingsPreferenceFragment {
     public int getMetricsCategory() {
         return MetricsEvent.PIXELDUST;
     }
+
+       public static void reset(Context mContext) {
+       final ContentResolver resolver = mContext.getContentResolver();
+       Settings.System.putIntForUser(resolver,
+                Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE, 4, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_LAYOUT_COLUMNS, 2, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_TILE_VERTICAL_LAYOUT, 0, UserHandle.USER_CURRENT);
+        Settings.System.putIntForUser(resolver,
+                Settings.System.QS_TILE_LABEL_HIDE, 0, UserHandle.USER_CURRENT);
+     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
