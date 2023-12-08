559847f1b2ef3502cf964ac50a4840fac91b4644
LurdRaiden <everethduncan@gmail.com>
2023-12-07 22:07:42 -0500

    Introduce DotOS dashboard style [1/2]

--- src/com/pixeldust/settings/themes/ThemeSettings.java
+++ src/com/pixeldust/settings/themes/ThemeSettings.java
@@ -5,20 +5,80 @@
  */
 
 package com.pixeldust.settings.themes;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.Settings;
 
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.Preference;
+
+import androidx.preference.ListPreference;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
+import com.android.internal.util.custom.ThemeUtils;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
+import androidx.preference.Preference;
+import android.os.Bundle;
+import android.content.Context;
+import androidx.preference.PreferenceScreen;
 
 import java.util.Arrays;
 import java.util.List;
 
 @SearchIndexable
-public class ThemeSettings extends DashboardFragment {
+public class ThemeSettings extends DashboardFragment implements OnPreferenceChangeListener {
 
     private static final String TAG = "ThemeSettings";
+    private static final String KEY_QS_PANEL_STYLE  = "qs_panel_style";
+    private static final String KEY_DASHBOARD_STYLE = "settings_dashboard_style";
+
+    private ListPreference mDashBoardStyle;
+    private Handler mHandler;
+    private ListPreference mQsStyle;
+    private ThemeUtils mThemeUtils;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+         final PreferenceScreen prefScreen = getPreferenceScreen();
+        mThemeUtils = new ThemeUtils(getActivity());
+
+        mQsStyle = (ListPreference) findPreference(KEY_QS_PANEL_STYLE);
+        mCustomSettingsObserver.observe();
+        mDashBoardStyle = (ListPreference) prefScreen.findPreference(KEY_DASHBOARD_STYLE);
+        mDashBoardStyle.setOnPreferenceChangeListener(this);
+    }
+
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+    private class CustomSettingsObserver extends ContentObserver {
+
+        CustomSettingsObserver(Handler handler) {
+            super(handler);
+        }
+
+        void observe() {
+            Context mContext = getContext();
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_PANEL_STYLE),
+                    false, this, UserHandle.USER_ALL);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            if (uri.equals(Settings.System.getUriFor(Settings.System.QS_PANEL_STYLE))) {
+                updateQsStyle();
+            }
+        }
+    }
 
     @Override
     public int getMetricsCategory() {
@@ -28,6 +88,47 @@ public class ThemeSettings extends DashboardFragment {
     @Override
     protected String getLogTag() {
         return TAG;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mQsStyle) {
+           mCustomSettingsObserver.observe();
+           return true;
+        }
+        if (preference == mDashBoardStyle) {
+            ThemeUtils.showSettingsRestartDialog(getContext());
+            return true;
+        }
+        return false;
+    }
+
+    private void updateQsStyle() {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        int qsPanelStyle = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.QS_PANEL_STYLE , 0, UserHandle.USER_CURRENT);
+
+        switch (qsPanelStyle) {
+            case 0:
+              setQsStyle("com.android.systemui");
+              break;
+            case 1:
+              setQsStyle("com.android.system.qs.outline");
+              break;
+            case 2:
+              setQsStyle("com.android.system.qs.twotoneaccent");
+              break;
+            case 3:
+              setQsStyle("com.android.system.qs.shaded");
+              break;
+            default:
+              break;
+        }
+    }
+
+    public void setQsStyle(String overlayName) {
+        mThemeUtils.setOverlayEnabled("android.theme.customization.qs_panel", overlayName, "com.android.systemui");
     }
 
     @Override
